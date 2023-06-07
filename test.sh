67a7f9c40d7dcb84d325741c6b4fd69ff637cd1b
japrozs <sainijaproz@gmail.com>
2023-06-07 21:03:03 +0530

    :memo: fix bug in test script

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
 
-for file in samples/*; do
+for file in samples/*.class; do
 	./out/rum $file > /dev/null
 	if [ $? -eq 0 ]; then
 		echo ✅ $file
 	else
-	echo no
 		echo ❌ $file
 	fi
 done;
