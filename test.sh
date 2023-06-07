0b4d90116f14251c7aea43eb3e6caff5ba66f2f8
japrozs <sainijaproz@gmail.com>
2023-06-08 01:22:28 +0530

    fix testing script

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
-
+EXIT_CODE=0
 for file in samples/*.class; do
 	./out/rum $file > /dev/null
 	if [ $? -eq 0 ]; then
 		echo ✅ $file
 	else
 		echo ❌ $file
+		EXIT_CODE=1
 	fi
 done;
+
+exit $EXIT_CODE
